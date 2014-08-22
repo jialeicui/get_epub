9f35f807f75fa3b4e5674c6c6ae72884163bb920
jialei3 <jialei3@staff.sina.com.cn>
2014-08-22 18:16:12 +0800

    m

--- get_from_baidu.py
+++ get_from_baidu.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from urllib2 import urlopen, Request
+import urllib2
 import gzip
 import cStringIO
 import re
 import HTMLParser
 import sys
+import cookielib
+
+cookie=cookielib.CookieJar()
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
+user_agent='Mozilla/5.0 (iPhone; CPU iPhone OS 5_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Mobile/9B176 MicroMessenger/4.3.2'
+opener.addheaders = [('User-agent', user_agent)]
 
 def open_url(url):
-    req=Request(url)
-    user_agent='Mozilla/5.0 (iPhone; CPU iPhone OS 5_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Mobile/9B176 MicroMessenger/4.3.2'
-    req.add_header('User-agent', user_agent)
-
     try:
-        resp=urlopen(req,None,60)
+        resp=opener.open(url, None)
+
         # status code 200 - 'ok'.
         if resp.code==200:
             html = resp.read()
@@ -31,7 +34,8 @@ def open_url(url):
 def get_real_url(url):
     import httplib
     conn = httplib.HTTPConnection("m.baidu.com")
-    conn.request("HEAD", '/from=0/bd_page_type=1/ssid=0/uid=0/pu=usm%400%2Csz%40224_220%2Cta%40iphone___3_537/baiduid=DBF544B46BB2ECE56F3B647B45AA9BFC/w=0_10_%E5%A4%A7%E4%B8%BB%E5%AE%B0/t=iphone/l=1/tc?ref=www_iphone&lid=12283152388180642766&order=1&fm=alnv_book&tj=wise_novel_book_1_0_10_l1&sec=40826&di=406d01392c50e6f3&bdenc=1&nsrc=IlPT2AEptyoA_yixCFOxXnANedT62v3IGtiTNCVUB8SxokDyqRLrIxEtRD0fQGqPZpLRxWq4aNwYwn0wRjIz7aZ1t_Nkq7Eh9nvcgPruw2PKCBVdfhZmPMHUS7VrtOTam3cvdt2yF1EzAGcR8rS6t3stfQD3atBcndHSriymqfa02lmzD93ZnFrkZFw4HyGsDdjDaMylrm9dHZa_')
+    url = url[len('http://m.baidu.com'):]
+    conn.request("GET", url)
     res = conn.getresponse()
     res = res.getheaders()
 
@@ -50,16 +54,16 @@ def get_bookurl(bookname):
     query = open_url(url)
     res = re.search('<div class="box-card">(.*?)</div>', query).group(1)
     url = re.search('<a href="(.*?)"', res).group(1)
+    url = HTMLParser.HTMLParser().unescape(url)
 
-    print get_real_url(url)
-    sys.exit(0)
-    # return get_real_url(res)
+    return get_real_url(url)
     pass
 
 def get_chapter_info(bookname):
     url = get_bookurl(bookname)
     content = open_url(url)
-    res = re.findall('<dd><a href="(.*)"(.*)</a></dd>', content, re.U)
+    print content
+    res = re.findall('', content, re.U)
     return res
     pass
 
@@ -75,10 +79,12 @@ def get_chapter(url):
 
 def get_book(bookname):
     chapters = get_chapter_info(bookname)
+    print chapters
+    sys.exit(0)
     chapters = chapters[3:]
     for url,name in chapters:
         c = get_chapter(url)
-        open('cc.txt', 'w').write(c)
+
         return
     pass
 
